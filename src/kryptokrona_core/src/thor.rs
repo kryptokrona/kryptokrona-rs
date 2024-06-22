8b24da7c636fb8a65a3193a361e2e5c98915bad4
mjovanc <mjovanc@icloud.com>
2024-06-22 11:01:48 +0200

    Renaming Node

--- src/kryptokrona_core/src/thor.rs
+++ src/kryptokrona_core/src/thor.rs
@@ -1,9 +1,37 @@
+// Copyright (c) 2019-2024, The Kryptokrona Project
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 use std::{
     thread,
     time::Duration,
 };
 
-pub struct Node {
+pub struct Thor {
     pub timeout: Duration,
     pub daemon_host: String,
     pub daemon_port: i8,
@@ -18,7 +46,7 @@ pub struct Node {
     pub background_thread: Option<thread::JoinHandle<()>>,
 }
 
-impl Node {
+impl Thor {
     fn new(daemon_host: String, daemon_port: i8) -> Self {
         let timeout = Duration::from_secs(10);
         // TODO: create a new Hyper Client here
@@ -27,7 +55,7 @@ impl Node {
         //     timeout,
         // )));
 
-        Node {
+        Thor {
             timeout,
             daemon_host,
             daemon_port,
